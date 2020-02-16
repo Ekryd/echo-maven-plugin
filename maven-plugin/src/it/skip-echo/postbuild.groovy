b517bcef06e9dbf81ff7050dfb7cf8c84cfd535b
Björn Ekryd <bjornatekryd.se>
2020-02-16 22:41:01 +0100

    Fixed integration tests

--- maven-plugin/src/it/skip-echo/postbuild.groovy
+++ maven-plugin/src/it/skip-echo/postbuild.groovy
@@ -1,5 +1,5 @@
 log = new File(basedir, 'build.log')
-expected_log = new File(basedir, 'expected.log')
+expected_log = new File(basedir, 'not-expected.log')
 
 assert log.exists()
 assert !log.text.replaceAll('\r','').contains(expected_log.text.replaceAll('@pom.version@', projectversion).replaceAll('\r',''))
